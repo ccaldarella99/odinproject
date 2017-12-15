816dd7ce9b66a238f1ef981b721f62d978727ed9
Christopher <christopher.caldarella@outlook.com>
2017-12-15 15:48:35 -0700

    update Etch-a-sketch
    made some changes

--- etch-a-sketch101/app.js
+++ etch-a-sketch101/app.js
@@ -1,5 +1,6 @@
 const CANVAS_SIZE = 576;
 let startNumberOfBoxes = 6;
+let numPx = 0;
 
 const container = document.querySelector('#container');
 container.setAttribute('style', 'width:' + CANVAS_SIZE + 'px; height:' + CANVAS_SIZE + 'px; background-color:#ddd;');
@@ -18,7 +19,8 @@ function clearBoxes() {
 	let all = [];
 	all = container.querySelectorAll('div');
 	for(let i=0; i<all.length; i++) {
-		container.removeChild(all[i]);
+		//this.setAttribute('style','background-color:#ddd;');
+		//container.removeChild(all[i]);//remove
 	}
 }
 
@@ -33,7 +35,7 @@ function makeBtn() {
 }
 
 function shadeBox() {
-		div.setAttribute('style','background-color:#333;');
+			this.setAttribute('style','color:#fff; background-color:#ddd; border:0px #00f solid; width:' + numPx + 'px; height:' + numPx + 'px; margin:0; padding:0px; display:inline-block;');
 }
 
 function makeBoxes(numBoxes) {
@@ -47,7 +49,6 @@ function makeBoxes(numBoxes) {
 function setAttr(numBoxes) {
 	const div = document.createElement('div');
 	let boxNum = numBoxes;
-	let numPx = 0;
 
 	if(numBoxes > 192) {
 		boxNum = 192;
@@ -57,15 +58,10 @@ function setAttr(numBoxes) {
 	}
 
 	div.setAttribute('style','color:#fff; background-color:#aaa; border:0px #00f solid; width:' + numPx + 'px; height:' + numPx + 'px; margin:0; padding:0px; display:inline-block;');
-	div.addEventListener('onclick', shadeBox);
-
-	for (let i=0; i<numBoxes;i++) {
-		//row-container
-		for (let j=0; j<numBoxes; j++) {
-			div.classList.add('content');
-			container.appendChild(div);
-		}
-	}
+	div.addEventListener('mouseover', shadeBox);
+//	div.addEventListener('click', shadeBox);
+	div.classList.add('content');
+	container.appendChild(div);
 }
 
 
