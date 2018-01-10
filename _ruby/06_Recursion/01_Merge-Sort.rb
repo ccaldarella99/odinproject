eea8f456a1324e7628f03c6b8f1d82697519e190
Christopher <christopher.caldarella@outlook.com>
2018-01-09 17:25:52 -0700

    Fixed Merge-Sort
    Got Merge Sort to work
    (Length of split arrays needed to be recalculated)

--- _ruby/06_Recursion/01_Merge-Sort.rb
+++ _ruby/06_Recursion/01_Merge-Sort.rb
@@ -4,20 +4,26 @@ array2 = [8,7,6,5,4,3,2,1]
 array3 = [6,5,3,1,8,7,2,4,12]
 array4 = [1,2,2,9,5,22,4,11,16,4,5]
 array5 = [3,26,13,8,4,19,16,4,5,9,3,13]
+array9 = [2,9,5]
 
 def merge_sort(fullArr)
 	len = fullArr.length
+	halfLen = len/2
 	return fullArr.flatten if(len == 1)
 	
-	len1 = len/2
-	len2 = len/2
+	arr1 = fullArr[0..(halfLen-1)].flatten
+	arr2 = fullArr[halfLen..fullArr.length-1].flatten
+	len1 = arr1.length
+	len2 = arr2.length
 	
-	arr1 = fullArr[0..(len1-1)].flatten
-	arr2 = fullArr[len2..fullArr.length-1].flatten
 	newArr = []
 	newArr1 = []
 	newArr2 = []
-
+	
+#	puts "1: #{arr1}"
+#	puts "2: #{arr2}"
+#		puts "One and One: #{arr1} and #{arr2}"
+	
 	if(len1 == 1 && len2 == 1)
 		arr1.flatten!
 		arr2.flatten!
@@ -32,6 +38,9 @@ def merge_sort(fullArr)
 	newArr1.flatten!
 	newArr2.flatten!
 
+#	puts "3: #{newArr1}"
+#	puts "4: #{newArr2}"
+	
 	if(newArr1 != [nil] || newArr2 != [nil])
 		i=0
 		while (i<len && newArr1.length > 0 && newArr2.length > 0)# && (newArr1[0] != nil || newArr2[0] != nil))
@@ -44,10 +53,10 @@ def merge_sort(fullArr)
 			end
 			
 			if(newArr1 == [])
-				newArr << newArr2
+				newArr << merge_sort(newArr2)
 			end
 			if(newArr2 == [])
-				newArr << newArr1
+				newArr << merge_sort(newArr1)
 			end
 			
 			i += 1
@@ -58,7 +67,7 @@ def merge_sort(fullArr)
 		end
 	end
 	
-	newArr#.flatten!
+	newArr.flatten!
 end
 
 p array1
