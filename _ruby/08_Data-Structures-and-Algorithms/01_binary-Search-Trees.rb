5da9d7da65450fb62dbbcfe4b4da77233d25874d
Christopher <christopher.caldarella@outlook.com>
2018-01-23 23:18:16 -0700

    Finished first part of Binary Search Tree

--- _ruby/08_Data-Structures-and-Algorithms/01_binary-Search-Trees.rb
+++ _ruby/08_Data-Structures-and-Algorithms/01_binary-Search-Trees.rb
@@ -7,44 +7,60 @@ class Node
 		childLeft = nil
 		childRight = nil
 	end
+  
+end
+
+def depth_first_search
+end
+
+def dfs_rec
 end
 
 def build_tree(array)
-	
 	head = Node.new(array[0], nil)
 	current = head
 	i = 1
 
-#	puts current.value
+ 	while(i < array.length)
+    if(current.value > array[i] )
+      if(current.childLeft == nil)
+        current.childLeft = Node.new(array[i], current)
+        i += 1
+        current = head
+      else
+        current = current.childLeft
+      end
+    else
+      if(current.childRight == nil)
+        current.childRight = Node.new(array[i], current)
+        i += 1
+        current = head
+      else
+        current = current.childRight
+      end
+    end
+  end
 	
-	while(i < array.length)
-		if(current.childLeft == nil)
-			current.childLeft = Node.new(array[i], current)
-			i += 1
-#			puts current.childLeft.value
-		elsif (current.childRight == nil)
-			current.childRight = Node.new(array[i], current)
-			i += 1
-#			puts current.childRight.value
-		elsif(current.parent != nil && current.parent.childRight.childLeft == nil)
-			current = current.parent.childRight
-		elsif(current.parent != nil && current.parent.childRight.childRight == nil)
-			current = current.parent.childRight
-		elsif(current.parent != nil && current.parent.childLeft.childLeft == nil)
-			current = current.parent.childLeft
-		elsif(current.parent != nil && current.parent.childLeft.childRight == nil)
-			current = current.parent.childLeft
-		elsif(current.childLeft.childLeft == nil)
-			current = current.childLeft
-		end
-			
-	end
+  p array
+	puts "                   #{head.value}"
+	puts "                   |        #{head.childRight.value}"
+	puts "          |            #{head.childRight.childLeft.value}   |   #{head.childRight.childRight.value}"
 	
-	puts "        #{head.value}"
-	puts "    #{head.childLeft.value}   |   #{head.childRight.value}"
-	puts "#{head.childLeft.childLeft.value} | #{head.childLeft.childRight.value} | #{head.childRight.childLeft.value} | #{head.childRight.childRight.value}"
+#	print "    #{head.childLeft.childLeft.childLeft.value} | #{head.childLeft.childLeft.childRight.value}  "#{head.childLeft.childRight.childLeft.value} | #{head.childLeft.childRight.childRight.value}"
+  print "       |     |      "
+	puts " #{head.childRight.childLeft.childLeft.value} | #{head.childRight.childLeft.childRight.value}   #{head.childRight.childRight.childLeft.value} | #{head.childRight.childRight.childRight.value}"
+  print "    |     |     |   "
+	puts " |   |   |    | #{head.childRight.childRight.childRight.childRight.value}"
+  print "  |   | |   | |   |"
+	puts "|  | | | | |  |   | #{head.childRight.childRight.childRight.childRight.childLeft.value}"
+
+
+  #	puts "           #{head.childLeft.value}       |        #{head.childRight.value}"
+#	puts "       #{head.childLeft.childLeft.value}  |  #{head.childLeft.childRight.value}         #{head.childRight.childLeft.value}   |   #{head.childRight.childRight.value}"
 	
-	puts " #{head.childRight.childLeft.childLeft.value} | #{head.childRight.childLeft.childRight.value} | #{head.childRight.childRight.childLeft.value} | #{head.childRight.childRight.childRight.value}"
+#	print "    #{head.childLeft.childLeft.childLeft.value} | #{head.childLeft.childLeft.childRight.value}  "#{head.childLeft.childRight.childLeft.value} | #{head.childLeft.childRight.childRight.value}"
+#  print "         "
+#	puts " #{head.childRight.childLeft.childLeft.value} | #{head.childRight.childLeft.childRight.value}   #{head.childRight.childRight.childLeft.value} | #{head.childRight.childRight.childRight.value}"
 	
 	
 end
