fdd94bc3ed70597729068ae932e2a051beb56f90
ccaldarella99 <christopher.caldarella@bec.local>
2018-01-29 17:28:46 -0700

    Updating folders

---
+++ _ruby/08_Data-Structures-and-Algorithms/02_knights-Travails.rb
@@ -0,0 +1,9 @@
+
+def knight_moves(start_sq, end_sq)
+	moves = 0
+	puts "You made it in #{moves}! Here is your path:"
+end
+
+knight_moves([0,0], [1,2])
+knight_moves([0,0], [3,3])
+knight_moves([3,3], [0,0])
