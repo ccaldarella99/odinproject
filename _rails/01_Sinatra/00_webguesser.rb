7870028b5609fe8287b2e6e416901e967ec61e92
ccaldarella99 <christopher.caldarella@outlook.com>
2018-02-04 12:00:37 -0700

    I3: number guessing game is complete

--- _rails/01_Sinatra/00_webguesser.rb
+++ _rails/01_Sinatra/00_webguesser.rb
@@ -2,9 +2,27 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 random_number = rand(0..100)
+output = ""
+
+def check_guess(guess, random_number)
+  out = guess
+  if(guess == nil || guess == "")
+    return out
+  elsif(guess.to_i > random_number)
+    out += " IS TOO DAMN HIGH! <br><img src =\'https://tinyurl.com/y8ecfp7m\'>"
+  elsif(guess.to_i < random_number)
+    out += " is too low! <br><img src =\'https://tinyurl.com/yac7rhjr\'>"
+  else
+    out += " is correct! <br><img src=\'https://tinyurl.com/znjv4yj\'>"
+    out += "<br>The SECRET NUMBER is: #{random_number}"
+  end
+  out
+end
 
 
 get '/' do
-  erb :index, :locals => {:random_number => random_number}
+  guess = params["guess"]
+  output = check_guess(guess, random_number)
+  erb :index, :locals => {:random_number => random_number, :output => output}
 end
 
