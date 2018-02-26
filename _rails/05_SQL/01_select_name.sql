a25e2bbc8c1e8e342c8a8a4c2f630293f8bfb19f
ccaldarella99 <christopher.caldarella@outlook.com>
2018-02-25 20:48:06 -0700

    Finished Sinatra, starting SQL exercises

---
+++ _rails/05_SQL/01_select_name.sql
@@ -0,0 +1,50 @@
+
+
+# 1.  Find the country that start with Y
+SELECT name FROM world
+  WHERE name LIKE 'Y%'
+
+
+# 2.  Find the countries that end with y
+SELECT name FROM world
+  WHERE name LIKE '%Y'
+
+# 3.  
+
+
+# 4.  
+
+
+# 5.  
+
+
+# 6.  
+
+
+# 7.  
+
+
+# 8.  
+
+
+# 9.  
+
+
+# 10.  
+
+
+# 11.  
+
+
+# 12.  
+
+
+# 13.  
+
+
+# 14.  
+
+
+# 15.  
+
+
